f84b806fc024c1cb9462bcdb2a68ff26c850aeb2
1stephen37 <ngamingyahoo@gmail.com>
2024-07-10 19:45:30 +0700

    ss1

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -4,12 +4,14 @@ import ProductService from "../../services/products/products.service";
 import GalleryService from "../../services/gallery/gallery.service";
 import BrandsService from "../../services/brands/brands.service";
 import OptionsService from "../../services/options/options.service";
-import { Op } from 'sequelize';
+import {Op} from 'sequelize';
 
 const ProductsController = Router();
 
 ProductsController.get('', async (req: Request, res: Response) => {
     try {
+        let status = req.query.status;
+        const page = req.query.page;
         const id_brand = req.query.id_brand;
         const sale = req.query.sale;
         const offset = req.query.offset;
@@ -17,7 +19,12 @@ ProductsController.get('', async (req: Request, res: Response) => {
         const sort = req.query.sort;
         const property = req.query.property;
         const keyword = req.query.keyword;
-        let filter: { where?: { id_brand? : string, name? : {}}, offset?: number, limit?: number, order?: [[string, string]] } = {};
+        let filter: {
+            where?: { id_brand?: string, name?: {}, status?: number },
+            offset?: number,
+            limit?: number,
+            order?: [[string, string]]
+        } = {};
         if (offset) filter.offset = parseInt(offset as string);
         if (limit) filter.limit = parseInt(limit as string);
         if (sale) filter.order = [
@@ -27,35 +34,62 @@ ProductsController.get('', async (req: Request, res: Response) => {
             [property.toString(), sort.toString()]
         ];
         if (id_brand) {
-            if(filter.where) {
+            if (filter.where) {
                 filter.where.id_brand = id_brand.toString();
             } else {
                 filter.where = {
-                    id_brand : id_brand.toString()
+                    id_brand: id_brand.toString()
                 };
             }
         }
-        if(keyword) {
-            if(filter.where) {
+        if (keyword) {
+            if (filter.where) {
                 filter.where.name = {
                     [Op.iLike]: `%${keyword}%` // Sử dụng iLike để không phân biệt hoa/thường
                 }
             } else {
                 filter.where = {
-                    name : {
+                    name: {
                         [Op.iLike]: `%${keyword}%`
                     }
                 };
             }
         }
-        let products = await ProductService.FindAllProducts(filter);
+        if (status) {
+            if (filter.where) {
+                filter.where.status = parseInt(status.toString())
+            } else {
+                filter.where = {
+                    status: parseInt(status.toString())
+                };
+            }
+        } else {
+            if (filter.where) {
+                filter.where.status = 1
+            } else {
+                filter.where = {
+                    status: 1
+                };
+            }
+        }
+        let count = 0;
+        if (page && !id_brand) {
+            count = await ProductService.getCountProductsByStatus(1);
+        } else if (page && status && !id_brand) {
+            count = await ProductService.getCountProductsByStatus(parseInt(status.toString()));
+        } else if (page && id_brand) {
+            count = await ProductService.getCountProductsByIdBrand(id_brand.toString());
+        }
+        console.log(count)
+        let products = await ProductService.FindProductsByFilter(filter);
         for (const index in products) {
             const gallery = await GalleryService.FindPrimaryGalleryImageByIdProduct(products[index].id_product);
             if (gallery) products[index].image = gallery.url;
             const brand = await BrandsService.FindNameBrandById(products[index].id_brand);
             if (brand) products[index].brand_name = brand.name;
-            const options = await OptionsService.findOneOptionBasicByIdProduct(products[index].id_product, ['id_option', 'id_product', 'price', 'memory']);
+            const options = await OptionsService.findOneOptionBasicByIdProduct(products[index].id_product, ['color', 'price', 'memory']);
             if (options) {
+                products[index].color = options.color;
                 products[index].memory = options.memory;
                 products[index].price = options.price;
             }
@@ -66,10 +100,12 @@ ProductsController.get('', async (req: Request, res: Response) => {
             products = products.sort((a, b) => b.price - a.price);
         }
         res.status(HttpStatus.SUCCESS).json({
-            data: products
+            data: products,
+            paging: {
+                total: count
+            }
         })
     } catch (error: Error | any) {
-        console.log(error.message)
         res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({message: error.message})
     }
 })
@@ -81,13 +117,13 @@ ProductsController.get('/detail/:id', async (req: Request, res: Response) => {
             res.status(HttpStatus.NO_CONTENT).json({message: 'don\'t have a id to find product'})
         }
         let product = await ProductService.getOneProductPyPk(id);
-        if(product) {
+        if (product) {
             const gallery = await GalleryService.FindPrimaryGalleryImageByIdProduct(product.id_product);
             if (gallery) product.image = gallery.url;
             const brand = await BrandsService.FindNameBrandById(product.id_brand);
-            if(brand) product.brand_name = brand.name;
+            if (brand) product.brand_name = brand.name;
             const optionsList = await OptionsService.findAllOptionsByIdProduct(product.id_product);
-            if(optionsList) {
+            if (optionsList) {
                 console.log(optionsList)
             }
         }
--- src/services/products/products.service.ts
+++ src/services/products/products.service.ts
@@ -1,11 +1,25 @@
 import Products from "../../entities/products/products.entity";
 
 export default class ProductService {
-    static async FindAllProducts(filter : {} = { raw: true }) {
+    static async FindProductsByFilter(filter : {} = { raw: true }) {
         if(filter) filter = { ...filter, raw: true };
         return await Products.findAll(filter);
     }
     static async getOneProductPyPk(id_product : string) {
         return await Products.findByPk(id_product, { raw: true });
+    }
+    static async getCountProductsByStatus(status : number) {
+        return await Products.count({
+            where: {
+                status: status
+            }
+        })
+    }
+    static async getCountProductsByIdBrand(id_brand : string) {
+        return await Products.count({
+            where: {
+                id_brand: id_brand
+            }
+        })
     }
 }
